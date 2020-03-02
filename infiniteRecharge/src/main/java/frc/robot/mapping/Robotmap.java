cd12a700bef68546b0c2b07e799521c1f49322a1
Jacob Lewis <54906247+Stikcyee@users.noreply.github.com>
2020-03-01 21:01:11 -0600

    added proper names and motor controllers

--- infiniteRecharge/src/main/java/frc/robot/mapping/Robotmap.java
+++ infiniteRecharge/src/main/java/frc/robot/mapping/Robotmap.java
@@ -2,19 +2,25 @@ package frc.robot.mapping;
 
 public class Robotmap {
 
-    // Drivetrain.java
+    //DRIVETRAIN
+    //SPARK MAX
     public static final int _frontLeftMotorID = 7;
     public static final int _rearLeftMotorID = 8;
     public static final int _frontRightMotorID = 1;
     public static final int _rearRightMotorID = 4;
 
-    // subsystems/Manipulator.java
+    //INTAKE
+    //TALON
     public static final int _intakeMotorID = 16;
+    //SHOOTER
+    //SPARK MAX
     public static final int _shooterMotorOneID = 6;
     public static final int _shooterMotorTwoID = 2;
 
-    //winch / climb
+    //WINCH / CLIMB
+    //SPARK MAX
     public static final int _winchLeft = 3;
     public static final int _winchRight = 5;
+    //TALON
     public static final int _slide = 12;
 }
