acc19827c6e177971ee15ff7b30464b441733f40
Jacob Lewis <54906247+Stikcyee@users.noreply.github.com>
2020-04-02 22:12:27 -0500

    Update BlinkinNeo.java

--- FiniteRecharge/src/main/java/frc/robot/input/BlinkinNeo.java
+++ FiniteRecharge/src/main/java/frc/robot/input/BlinkinNeo.java
@@ -3,7 +3,7 @@ package frc.robot.input;
 import edu.wpi.first.wpilibj.Spark;
 import frc.robot.mapping.Robotmap;
 
-//  http://www.revrobotics.com/content/docs/REV-11-1105-UM.pdf page 8 has the guide on why we use a shitty Spark
+//  http://www.revrobotics.com/content/docs/REV-11-1105-UM.pdf page 8 has the guide on why we use a Spark
 
 /**
  * @author Jacob Lewis
